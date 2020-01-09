dd969f7592e287ad5ef7e8a015ae84b568f698b1
Damir <damirz@gmail.com>
2020-01-09 15:34:33 +0000

    Add a spec to ensure Thursdays
    Added an initial implementation for `#dates` to return an array of dates and wrote a failing spec that every date should be a Thursday.

--- generate.rb
+++ generate.rb
@@ -1,5 +1,5 @@
 class Generate
   def dates
-    # []
+    2.times.map { Time.new(2020, 1, 1) }
   end
 end
--- spec/generate_spec.rb
+++ spec/generate_spec.rb
@@ -2,10 +2,16 @@ require_relative "../generate.rb"
 
 RSpec.describe Generate do
   describe "#dates" do
-    it "returns an Array" do
-      generate = Generate.new
+    let(:generate) { Generate.new }
+
+    it "returns an array of Time objects" do
       meetup_dates = generate.dates
-      expect(meetup_dates.class).to be(Array)
+      expect(meetup_dates).to be_any(Time)
+    end
+
+    it "returns a list of Thursdays" do
+      meetup_dates = generate.dates
+      expect(meetup_dates.map(&:wday)).to be_all(4)
     end
   end
 end
